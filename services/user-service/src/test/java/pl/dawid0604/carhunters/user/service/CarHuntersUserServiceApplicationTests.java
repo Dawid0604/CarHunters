761c6749b15c25c49404041642066b3f5792c544
Dawid Sikora <93526656+Dawid0604@users.noreply.github.com>
2025-06-19 23:08:55 +0200

    feat: Basic User Service Implementation (#16)

---
+++ services/user-service/src/test/java/pl/dawid0604/carhunters/user/service/CarHuntersUserServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package pl.dawid0604.carhunters.user.service;
+
+import lombok.NoArgsConstructor;
+import org.junit.jupiter.api.Test;
+
+import static lombok.AccessLevel.PACKAGE;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@NoArgsConstructor(access = PACKAGE)
+class CarHuntersUserServiceApplicationTests extends IntegrationTestBase {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
