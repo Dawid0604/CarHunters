d2e39b94c58dcec3a425da6f503a3bb3296af575
Dawid Sikora <tadek123tadek@o2.pl>
2025-06-20 18:56:32 +0200

    feat: Basic Notification Service Implementation

---
+++ services/notification-service/src/main/java/pl/dawid0604/carhunters/notification/service/CarHuntersNotificationServiceApplication.java
@@ -0,0 +1,22 @@
+package pl.dawid0604.carhunters.notification.service;
+
+import lombok.NoArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import static lombok.AccessLevel.PACKAGE;
+
+@EnableCaching
+@SpringBootApplication
+@EnableDiscoveryClient
+@SuppressWarnings("PMD")
+@NoArgsConstructor(access = PACKAGE)
+class CarHuntersNotificationServiceApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(CarHuntersNotificationServiceApplication.class, args);
+	}
+
+}
