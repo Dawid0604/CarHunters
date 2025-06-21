a073e140a25803b8000a59c6c6c338ddbcabb69f
Dawid Sikora <tadek123tadek@o2.pl>
2025-06-21 19:32:08 +0200

    feat: Basic CarHistory Service implementation

---
+++ services/car-history-service/src/main/java/pl/dawid0604/carhunters/car/history/service/config/migration/MongoMigrationConfig.java
@@ -0,0 +1,23 @@
+package pl.dawid0604.carhunters.car.history.service.config.migration;
+
+import io.mongock.runner.springboot.EnableMongock;
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+import static lombok.AccessLevel.PACKAGE;
+
+/**
+ * The configuration class enabling Mongock for
+ * MongoDB migrations.
+ * <p>
+ *     Mongock is a Java-based migration tool for
+ *     MongoDB, inspired by tools like Flyway. It
+ *     allows to manage database schema and data migrations
+ *     using Java code.
+ * </p>
+ */
+@EnableMongock
+@Configuration
+@SuppressWarnings({ "unused", "PMD.CommentSize" })
+@NoArgsConstructor(access = PACKAGE)
+class MongoMigrationConfig { }
---
+++ services/car-history-service/src/test/java/pl/dawid0604/carhunters/car/history/service/IntegrationTestBase.java
@@ -0,0 +1,92 @@
+package pl.dawid0604.carhunters.car.history.service;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.kafka.KafkaContainer;
+
+import static lombok.AccessLevel.PACKAGE;
+import static lombok.AccessLevel.PROTECTED;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static pl.dawid0604.carhunters.car.history.service.IntegrationTestBase.ContainersConfig;
+
+/**
+ * The base of all integration tests.
+ * <p>
+ *     Provides the server port for RestAssured and sets up
+ *     required infrastructure components, such as databases,
+ *     message brokers etc. This setup closely reflects
+ *     the real application state.
+ * </p>
+ *
+ * Extend this class in your integration tests to ensure proper
+ * environment configuration.
+ */
+@Getter(PROTECTED)
+@Import(ContainersConfig.class)
+@NoArgsConstructor(access = PACKAGE)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@SuppressWarnings({ "PMD.CommentSize", "PMD.AbstractClassWithoutAbstractMethod" })
+public abstract class IntegrationTestBase {
+
+    /**
+     * The port injected by framework for use in integration tests.
+     * <p>Example usage</p>
+     * <pre>
+     * {@code
+     *  RestAssured.given()
+     *             .port(serverPort) // this variable
+     *             .when();
+     * }
+     * </pre>
+     * <p>The port is assigned dynamically by framework during test startup.</p>
+     */
+    @LocalServerPort
+    private int serverPort;
+
+    /**
+     * Test configuration that provides containerized dependencies for
+     * integration tests.
+     *
+     * <p>
+     *     This class configures and exposes required dependencies as {@link Bean}, which
+     *     are used by tests running in the Spring Boot environment. Thanks to the
+     *     {@link ServiceConnection} annotation, the containers are automatically connected
+     *     as data sources for the application context.
+     * </p>
+     */
+    @TestConfiguration
+    @SuppressWarnings("PMD.CommentDefaultAccessModifier")
+    static class ContainersConfig {
+
+        /**
+         * Creates and configures the MongoDB container (Version 8.0.9) for
+         * integration tests.
+         * @return a new MongoDB container instance running in the environment.
+         */
+        @Bean
+        @ServiceConnection
+        @SuppressWarnings("unused")
+        MongoDBContainer mongoDBContainer() {
+            return new MongoDBContainer("mongo:8.0.9");
+        }
+
+        /**
+         * Creates and configures the Kafka container (Version 3.9.1) for
+         * integration tests.
+         * @return a new Kafka container instance running in the environment.
+         */
+        @Bean
+        @ServiceConnection
+        @SuppressWarnings("unused")
+        KafkaContainer kafkaContainer() {
+            return new KafkaContainer("apache/kafka:3.9.1");
+        }
+    }
+}
