8e0b12f11826169d63974605fdebca8b16e9af6d
Dawid Sikora <93526656+Dawid0604@users.noreply.github.com>
2025-06-21 20:10:27 +0200

    feat: Basic Car History Service implementation (#22)

---
+++ services/car-history-service/src/test/java/pl/dawid0604/carhunters/car/history/service/CarHuntersCarHistoryServiceApplicationTests.java
@@ -0,0 +1,24 @@
+package pl.dawid0604.carhunters.car.history.service;
+
+import lombok.NoArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+
+import static lombok.AccessLevel.PACKAGE;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@NoArgsConstructor(access = PACKAGE)
+@SuppressWarnings("PMD.CommentRequired")
+class CarHuntersCarHistoryServiceApplicationTests extends IntegrationTestBase {
+
+	@Autowired
+	@SuppressWarnings("unused")
+	private ApplicationContext context;
+
+	@Test
+	void contextLoads() {
+		assertThat(context).isNotNull();
+	}
+
+}
