a07999a9a66d17947772f143cf2bf5047155942e
Dawid Sikora <93526656+Dawid0604@users.noreply.github.com>
2025-06-25 19:39:09 +0200

    feat: Basic Gateway Service implementation (#25)

---
+++ services/gateway-service/src/test/java/pl/dawid0604/carhunters/gateway/service/CarHuntersGatewayServiceApplicationTests.java
@@ -0,0 +1,26 @@
+package pl.dawid0604.carhunters.gateway.service;
+
+import lombok.NoArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static lombok.AccessLevel.PACKAGE;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@NoArgsConstructor(access = PACKAGE)
+@SuppressWarnings("PMD.CommentRequired")
+class CarHuntersGatewayServiceApplicationTests {
+
+	@Autowired
+	@SuppressWarnings("unused")
+	private ApplicationContext context;
+
+	@Test
+	void contextLoads() {
+		assertThat(context).isNotNull();
+	}
+
+}
