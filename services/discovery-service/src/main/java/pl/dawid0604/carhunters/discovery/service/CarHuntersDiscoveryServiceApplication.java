203192a60a14ddb69e313ac67d18744193ce0ce2
Dawid Sikora <tadek123tadek@o2.pl>
2025-06-28 10:50:44 +0200

    chore: add missing docs [Discovery Service]

--- services/discovery-service/src/main/java/pl/dawid0604/carhunters/discovery/service/CarHuntersDiscoveryServiceApplication.java
+++ services/discovery-service/src/main/java/pl/dawid0604/carhunters/discovery/service/CarHuntersDiscoveryServiceApplication.java
@@ -5,12 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * Application runtime.
+ */
 @EnableCaching
 @EnableEurekaServer
 @SpringBootApplication
 @SuppressWarnings("PMD")
 class CarHuntersDiscoveryServiceApplication {
 
+	/**
+	 * Simple main method.
+	 * @param args arguments
+	 */
 	public static void main(final String[] args) {
 		SpringApplication.run(CarHuntersDiscoveryServiceApplication.class, args);
 	}
