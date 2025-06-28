c6281a5ad58d56a419a6700ad8728f1bc20b94fd
Dawid Sikora <tadek123tadek@o2.pl>
2025-06-28 10:51:15 +0200

    chore: replace global PMD suppressions with specific suppression [Discovery Service]

--- services/discovery-service/src/main/java/pl/dawid0604/carhunters/discovery/service/CarHuntersDiscoveryServiceApplication.java
+++ services/discovery-service/src/main/java/pl/dawid0604/carhunters/discovery/service/CarHuntersDiscoveryServiceApplication.java
@@ -11,7 +11,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableCaching
 @EnableEurekaServer
 @SpringBootApplication
-@SuppressWarnings("PMD")
+@SuppressWarnings("PMD.UseUtilityClass")
 class CarHuntersDiscoveryServiceApplication {
 
 	/**
