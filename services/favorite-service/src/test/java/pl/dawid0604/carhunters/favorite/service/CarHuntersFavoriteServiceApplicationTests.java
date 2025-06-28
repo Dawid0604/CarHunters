924ff53d3107a6a32c3aadfd1789ec80f48c1c90
Dawid Sikora <tadek123tadek@o2.pl>
2025-06-28 23:01:21 +0200

    chore: add missing docs [Favorite Service]

--- services/favorite-service/src/test/java/pl/dawid0604/carhunters/favorite/service/CarHuntersFavoriteServiceApplicationTests.java
+++ services/favorite-service/src/test/java/pl/dawid0604/carhunters/favorite/service/CarHuntersFavoriteServiceApplicationTests.java
@@ -2,18 +2,23 @@ package pl.dawid0604.carhunters.favorite.service;
 
 import lombok.NoArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 
 import static lombok.AccessLevel.PACKAGE;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @NoArgsConstructor(access = PACKAGE)
 @SuppressWarnings("PMD.CommentRequired")
 class CarHuntersFavoriteServiceApplicationTests extends IntegrationTestBase {
 
+	@Autowired
+	@SuppressWarnings("unused")
+	private ApplicationContext applicationContext;
+
 	@Test
-	@SuppressWarnings("PMD")
 	void contextLoads() {
-		assertTrue(true);
+		assertThat(applicationContext).isNotNull();
 	}
 
 }
