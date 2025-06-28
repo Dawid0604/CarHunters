f9f167971cffa2be768b2ccc2e7634cbed08b489
Dawid Sikora <tadek123tadek@o2.pl>
2025-06-28 10:49:59 +0200

    chore: add missing docs [Notification Service]

--- services/notification-service/src/main/java/pl/dawid0604/carhunters/notification/service/config/AuditingConfig.java
+++ services/notification-service/src/main/java/pl/dawid0604/carhunters/notification/service/config/AuditingConfig.java
@@ -10,12 +10,28 @@ import java.util.Optional;
 
 import static lombok.AccessLevel.PACKAGE;
 
-@SuppressWarnings("unused")
+/**
+ * The configuration class enabling entity auditing for MongoDB.
+ * <p>
+ *     Registers the necessary beans to support auditing features,
+ *     such as tracking who created or modified an entity. Currently,
+ *     retrieval is not implemented.
+ * </p>
+ */
 @Configuration
 @EnableMongoAuditing
+@SuppressWarnings("unused")
 @NoArgsConstructor(access = PACKAGE)
 class AuditingConfig {
 
+    /**
+     * Returns the identifier of the current auditor (e.g., username).
+     * <p>
+     *     This method is prepared for further development
+     * </p>
+     * @return empty {@link Optional}, since auditor
+     * retrieval is not yet completed
+     */
     @Bean
     public AuditorAware<String> auditorAware() {
         return Optional::empty;
